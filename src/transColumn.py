3a30dd59fc8c2fa8eecd15453ba24b298014a765
David P <dpearson1983@gmail.com>
2022-05-11 12:23:24 -0500

    Adding more functionality

--- src/transColumn.py
+++ src/transColumn.py
@@ -16,4 +16,15 @@ class column:
         self.median = np.median(self.data)
         self.stdev = np.std(self.data)
 
+    def set_cell(i, val):
+        self.data[i] = val
 
+    def fill_uniform_rand(self, a = 0, b = 1):
+        self.data = np.random.uniform(a,b,len(self.data))
+
+    def fill_normal_rand(self, mu = 0, sigma = 1):
+        self.data = np.random.normal(mu, sigma, len(self.data))
+
+    def fill_linear(a, b):
+        step = (b - a)/len(self.data)
+        self.data = np.linspace(a, b, step)
--- src/transSheet.py
+++ src/transSheet.py
@@ -7,3 +7,7 @@ class sheet:
         for i in range(0,columns):
             col = column(names[i], rows)
             self.table.append(col)
+
+    def set_equation(col_name, eq_str):
+        # Use the built in eval, but first parse column names used in equation
+        # USe globals(), local(), or exec() to set variable names from the stored column names
